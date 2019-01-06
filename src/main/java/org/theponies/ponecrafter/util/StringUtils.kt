5c49328487773128b5bb30c86c8abde2a21c292d
Michiel Bos <michielbos97@hotmail.com>
2019-01-06 16:06:37 +0100

    Deleted old code.

--- src/main/java/org/theponies/ponecrafter/util/StringUtils.kt
+++
@@ -1,8 +0,0 @@
-package org.theponies.ponecrafter.util
-
-object StringUtils {
-
-    fun capitalizeFirst(string: String): String {
-        return string.substring(0, 1).toUpperCase() + string.substring(1)
-    }
-}//Private constructor
