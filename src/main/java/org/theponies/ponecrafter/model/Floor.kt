4cbcf9137e74dc3baec6e4b29aa9d206dbb85c95
Michiel Bos <michielbos97@hotmail.com>
2019-01-06 17:34:36 +0100

    Floor is now a JsonModel.

--- src/main/java/org/theponies/ponecrafter/model/Floor.kt
+++ src/main/java/org/theponies/ponecrafter/model/Floor.kt
@@ -7,20 +7,39 @@ import javafx.scene.image.Image
 import org.theponies.ponecrafter.util.UuidUtil
 import tornadofx.*
 import java.util.*
+import javax.json.JsonObject
 
-class Floor(name: String = "", description: String = "", price: Int = 0) {
+class Floor(name: String = "", description: String = "", price: Int = 0) : JsonModel {
     val nameProperty = SimpleStringProperty(this, "name", name)
-    var name by nameProperty
+    var name: String by nameProperty
 
     val descriptionProperty = SimpleStringProperty(this, "description", description)
-    var description by descriptionProperty
+    var description: String by descriptionProperty
 
     val priceProperty = SimpleIntegerProperty(this, "price", price)
-    var price by priceProperty
+    var price: Int by priceProperty
 
     val imageProperty = SimpleObjectProperty<Image>(this, "image", null)
-    var image by imageProperty
+    var image: Image by imageProperty
 
     val uuidProperty = SimpleObjectProperty<UUID>(this, "uuid", UuidUtil.generateContentUuid())
-    var uuid by uuidProperty
+    var uuid: UUID by uuidProperty
+
+    override fun updateModel(json: JsonObject) {
+        with(json) {
+            uuid = uuid("uuid") ?: UUID(0, 0)
+            name = string("name") ?: ""
+            description = string("description") ?: ""
+            price = int("price") ?: 0
+        }
+    }
+
+    override fun toJSON(json: JsonBuilder) {
+        with(json) {
+            add("uuid", uuid)
+            add("name", name)
+            add("description", description)
+            add("price", price)
+        }
+    }
 }
