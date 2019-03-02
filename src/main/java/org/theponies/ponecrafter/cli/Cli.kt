4960f224e799c2e99c820ef8110b39d68d4e0e82
Michiel Bos <michielbos97@hotmail.com>
2019-03-02 16:01:44 +0100

    Improved CLI command structure so help entries are automatically added to the help command.

--- src/main/java/org/theponies/ponecrafter/cli/Cli.kt
+++ src/main/java/org/theponies/ponecrafter/cli/Cli.kt
@@ -1,20 +1,38 @@
 package org.theponies.ponecrafter.cli
 
 class Cli {
+    private val commands = mapOf(
+        Pair("help", Command("", "Display command help.", ::help)),
+        Pair("create", Command("<input file> (<output file>)", "Create a PCC file from a folder.", ::create))
+    )
+
     fun execute(args: Array<String>) {
         if (args.size < 2) {
             println("Usage: <executable> cli <command> [parameters]")
             println("Try 'cli help' for a list of commands.")
             return
         }
+        val command = args[1]
         val params = args.drop(2)
-        when (args[1]) {
-            "help" -> help()
+        if (commands.containsKey(command)) {
+            commands.getValue(command).callback.invoke(params)
+        } else {
+            println("Invalid command: $command")
+            println("Try 'cli help' for a list of commands.")
         }
     }
 
-    private fun help() {
+    private fun help(params: List<String>) {
         println("Usage: <executable> cli <command> [parameters]")
         println("CLI commands:")
+        commands.forEach {k, v ->
+            println("$k ${v.paramDescription}: ${v.description}")
+        }
     }
+
+    private fun create(params: List<String>) {
+
+    }
+
+    data class Command(val paramDescription: String, val description: String, val callback: (params: List<String>) -> Any)
 }
