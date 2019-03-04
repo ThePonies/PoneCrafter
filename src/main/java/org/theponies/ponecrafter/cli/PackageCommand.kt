160e8c0a6c5951723bc29d112039cd12005ca971
Michiel Bos <michielbos97@hotmail.com>
2019-03-04 19:40:28 +0100

    Committed placeholder package command to fix compiler issues, oops.

---
+++ src/main/java/org/theponies/ponecrafter/cli/PackageCommand.kt
@@ -0,0 +1,9 @@
+package org.theponies.ponecrafter.cli
+
+class PackageCommand(paramDescription: String, description: String) : Command(paramDescription, description) {
+
+    override fun execute(params: List<String>) {
+        println("placeholder command")
+    }
+
+}
